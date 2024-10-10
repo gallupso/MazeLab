0ce5da3e2e7fe0f0edc2cf211a024349d40fbe3e
gallupso <gallupso@uab.edu>
2024-10-10 16:10:50 -0500

    Update Labyrinth.cpp
    added random number

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -1,6 +1,8 @@
 #include "Labyrinth.h"
 
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
+
+    int randNum=0;
     /* TODO: Delete this comment and the remaining lines, then implement this function. */
     (void) start;
     (void) moves;

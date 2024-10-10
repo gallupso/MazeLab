ba2fe896f81d3aa6694654138f396e8d452d6b50
gallupso <gallupso@uab.edu>
2024-10-10 16:15:43 -0500

    Update Labyrinth.cpp
    Added 2nd rand num

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -3,6 +3,7 @@
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
 
     int randNum=0;
+    int randNum2=0;
     /* TODO: Delete this comment and the remaining lines, then implement this function. */
     (void) start;
     (void) moves;

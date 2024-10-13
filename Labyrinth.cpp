9d1cccc4070be1afb92bd5553d3ba05d3ea8a47c
zpguard <jpguard@uab.edu>
2024-10-13 14:52:43 -0500

    Started on isPathToFreedom
    Tried recursion to check if the path is illegal. Bool doesn't check for items yet. Untested w/ comments

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -1,11 +1,24 @@
 #include "Labyrinth.h"
 
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
+  //string looks like NSWE, and our pointers are named north,south,east,west. I'm trying to link them here so the function knows that if the string is N, it needs to check if a north pointer is null. This might be wrong bc they might already be linked, or bc this is incorrect syntax. 
+  N = north;
+  S = south;
+  E = east;
+  W = west;
+  //std::string nope = "This path is illegal ;
 
-    int randNum=0;
-    int randNum2=0;
-    /* TODO: Delete this comment and the remaining lines, then implement this function. */
-    (void) start;
-    (void) moves;
-    return false;
+  if (start == nullptr)
+    return "This path is illegal";
+
+  else {
+    counter += 1; //counts how many cells we have passed through 
+    //since moves is const, not sure if i can break this into a substr here and be able to pass it in 
+    std::string part = moves.substr(counter +1);
+    //'moves(counter)' represents the cell we are currently in,
+    //'part' contains the part of the moves string that we still need to check 
+    isPathToFreedom(moves(counter), part);
+  }
+  
+   
 }

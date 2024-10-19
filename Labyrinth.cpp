57ba5ab5fe11bf3dbdd52da5f49f19ed57b49bce
zpguard <jpguard@uab.edu>
2024-10-18 21:24:51 -0500

    Solution to twistyMazeLab
    edited Labyrinth.cpp to be more clear

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -38,31 +38,6 @@ bool isPathToFreedom(MazeCell* start, const std::string& moves) {
     return gotAllItems(GotSpellbook, GotPotion, GotWand);
 }
 
-    
-    //if (checkPath(start, GotSpellbook, GotPotion, GotWand)) {
-        
-        /*for (std::size_t myI = 0; myI<moves.size()-1; myI++) {
-
-            if (moves[myI] == 'N')
-            MazeCell* start = start->north;
-           // checkPath(start->north,GotSpellbook,GotPotion,GotWand);
-            if (moves[myI] == 'S')
-            MazeCell* start = start->south;
-            //checkPath(start->south,GotSpellbook,GotPotion,GotWand);
-            if (moves[myI] == 'E')
-            MazeCell* start = start->east;
-            //checkPath(start->east,GotSpellbook,GotPotion,GotWand);
-            if (moves[myI] == 'W'){
-              //  std::cout<<"west";
-            MazeCell* start = start->west;}
-            
-        }
-       } 
-        //checkPath(start,GotSpellbook,GotPotion,GotWand);
-        return gotAllItems(GotSpellbook,GotPotion,GotWand);
-
-    }*/
-//}
 
 //checks the cell we have moved into for items. if there's an item, it adds 1 to our counters.
 //this function should not have to do anything if theres nothing in the cell 
@@ -73,16 +48,13 @@ void checkPath(MazeCell* v,int& GotSpellbook, int& GotPotion, int& GotWand ) {
  //   }
     switch(v->whatsHere){
         case (Item::SPELLBOOK):
-           GotSpellbook=1;
-           std::cout<<"spell";
+           GotSpellbook=1;    
            break;
         case(Item::POTION):
           GotPotion=1;
-          std::cout<<"potion";
           break;
         case(Item::WAND):
             GotWand=1;
-          std::cout<<"wand";
           break;
         case(Item::NOTHING):
           break;
@@ -92,12 +64,5 @@ void checkPath(MazeCell* v,int& GotSpellbook, int& GotPotion, int& GotWand ) {
 
 bool gotAllItems(int GotSpellbook, int GotPotion, int GotWand) {
     return (GotSpellbook+GotPotion+GotWand==3);
-    /*if(GotSpellbook + GotPotion +GotWand!=3) {
-        return false;
-        //if return false we have not collected all items
-    }
-    if (GotSpellbook + GotPotion + GotWand==3) {
-        //std::cout<<"yes";
-        return true;
-    }*/
+  
 }
